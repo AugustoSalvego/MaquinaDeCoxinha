c47b40208a8f13eed2b0c55f00d748e8e5f6a665
Augustinho <Augustinho@DESKTOP-HTE9D7G>
2024-07-09 11:26:40 -0300

    Trabalho maquina de coxinha

---
+++ src/main/java/com/meuteste/TelaController.java
@@ -0,0 +1,47 @@
+package com.meuteste;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
+
+public class TelaController {
+    
+    @FXML
+    private TextField saidaEstoque;
+    @FXML
+    private TextField entradaAbastecer;
+    @FXML
+    private TextField entradaVenderQtd;
+    
+    private MaquinaCoxinha maquinaCoxinha = new MaquinaCoxinha();
+    @FXML
+    private void atualizarEstoque() {
+        saidaEstoque.setText(String.valueOf(maquinaCoxinha.getEstoque()));
+    }
+    
+    @FXML
+    private void abastecer() {
+            int quantidade = Integer.parseInt(entradaAbastecer.getText());
+            maquinaCoxinha.abastecer(quantidade);
+            atualizarEstoque();
+    }
+    
+    @FXML
+    private void venderUmaCoxinha() {
+        maquinaCoxinha.vender();
+        atualizarEstoque();
+    }
+    
+    @FXML
+    private void venderQtd() {
+            int quantidade = Integer.parseInt(entradaVenderQtd.getText());
+            maquinaCoxinha.vender(quantidade);
+            atualizarEstoque();
+    }
+    
+    @FXML
+    private void zerarMaquina() {
+        maquinaCoxinha.zerar();
+        atualizarEstoque();
+    }
+}
